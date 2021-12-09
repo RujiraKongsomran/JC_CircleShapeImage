e7fa1ce380aa5e6cc0ba391578c3719cd7c56ee4
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-12-09 12:28:45 +0700

    Rounded corner shape and border

--- app/src/main/java/com/rujirakongsomran/jc_circleshapeimage/MainScreen.kt
+++ app/src/main/java/com/rujirakongsomran/jc_circleshapeimage/MainScreen.kt
@@ -1,12 +1,15 @@
 package com.rujirakongsomran.jc_circleshapeimage
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -19,9 +22,13 @@ fun MainScreen() {
         contentDescription = "Circular Image",
         modifier = Modifier
             .size(300.dp)
-            .clip(CircleShape),
-
-        )
+            .clip(RoundedCornerShape(size = 50.dp))
+            .border(
+                width = 10.dp,
+                color = Color.Cyan,
+                shape = RoundedCornerShape(50.dp)
+            )
+    )
 }
 
 @Composable
