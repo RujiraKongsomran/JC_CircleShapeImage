1282515225ed61cacbda3d748ca7c467ed36afb7
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-12-09 12:22:43 +0700

    Create circular image

---
+++ app/src/main/java/com/rujirakongsomran/jc_circleshapeimage/MainScreen.kt
@@ -0,0 +1,33 @@
+package com.rujirakongsomran.jc_circleshapeimage
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.rujirakongsomran.jc_circleshapeimage.ui.theme.JC_CircleShapeImageTheme
+
+@Composable
+fun MainScreen() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_launcher_background),
+        contentDescription = "Circular Image",
+        modifier = Modifier
+            .size(300.dp)
+            .clip(CircleShape),
+
+        )
+}
+
+@Composable
+@Preview
+fun MainScreenPreview() {
+    JC_CircleShapeImageTheme {
+        MainScreen()
+    }
+}
